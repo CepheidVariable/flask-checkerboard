90f0ae8e0887020d4f0f14074bae664dbb7487c2
--global <--global>
2021-10-21 18:28:55 -0500

    optimized color selection in routes

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import random
 
 app = Flask(__name__)
 
+def rand_coord():
+    return round(random.random()*(20-1) + 1)
+
+def rand_color():
+    return round(random.random()*255 - 0)
+
 @app.errorhandler(Exception)
 def errorhandler(err):
     app.logger.exception(err)
@@ -13,23 +19,21 @@ def errorhandler(err):
 @app.route('/<int:x>/<int:y>')
 @app.route('/<int:x>/<int:y>/<color1>')
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
-def gen_checkerboard(x= 8, y= 8, color1= "255,0,0", color2= "0,0,0"):
+def gen_checkerboard(x= 8, y= 8, color1= "red", color2= "black"):
     return render_template("index.html", x= x, y= y, color1= color1, color2= color2)
 
 @app.route('/r')
 def random_board():
-    x= random.random()*(20-1) + 1
-    y= random.random()*(20-1) + 1
-    color1= ""
-    color2= ""
 
-    for i in range(3):
-        color1 += str(round(random.random()*255 - 0)) + ","
-        color2 += str(round(random.random()*255 - 0)) + ","
-    color1 = color1[:-1]
-    color2 = color2[:-1]
+    rand_num_pool = []
 
-    return render_template("index.html", x= int(x), y= int(y), color1= color1, color2= color2)
+    for i in range(6):      
+        rand_num_pool.append(rand_color())
+
+    color1 = f"rgb({','.join(str(x) for x in rand_num_pool[:3])})"
+    color2 = f"rgb({','.join(str(x) for x in rand_num_pool[-3:])})"
+
+    return render_template("index.html", x= rand_coord(), y= rand_coord(), color1= color1, color2= color2)
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
