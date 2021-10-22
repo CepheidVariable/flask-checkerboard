279df939b719f3b5aa2ac967d523fa6723ac1dbe
--global <--global>
2021-10-22 08:49:34 -0500

    minor: more explicit func names

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ def gen_checkerboard(x= 8, y= 8, color1= "red", color2= "black"):
 def random_board():
 
     #random coordinate for x,y
-    c = lambda : round(random.random()*(20-1) + 1)
+    get_coord = lambda : round(random.random()*(20-1) + 1)
 
     #random rgb sub value (i.e. rgb(r,r,r))
-    r = lambda : (round(random.random()*255 - 0))
+    get_rgb = lambda : round(random.random()*255 - 0)
 
     #generate string to pass as CSS background-color property
-    color1 = f"rgb({r()}, {r()}, {r()})"
-    color2 = f"rgb({r()}, {r()}, {r()})"
+    color1 = f"rgb({get_rgb()}, {get_rgb()}, {get_rgb()})"
+    color2 = f"rgb({get_rgb()}, {get_rgb()}, {get_rgb()})"
 
-    return render_template("index.html", x= c(), y= c(), color1= color1, color2= color2)
+    return render_template("index.html", x= get_coord(), y= get_coord(), color1= color1, color2= color2)
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
