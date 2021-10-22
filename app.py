77fca6ddee2795badefff430e22c51cd967b39e4
--global <--global>
2021-10-21 22:38:48 -0500

    added comments; more explicit variable names

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import random
 
 app = Flask(__name__)
 
-def rand_coord():
+def rand_coord_value():
     return round(random.random()*(20-1) + 1)
 
-def rand_color():
+def rand_color_value():
     return round(random.random()*255 - 0)
 
 @app.errorhandler(Exception)
@@ -25,15 +25,16 @@ def gen_checkerboard(x= 8, y= 8, color1= "red", color2= "black"):
 @app.route('/r')
 def random_board():
 
+    #random numbers to create two sets of rgb values
     rand_num_pool = []
-
     for i in range(6):      
-        rand_num_pool.append(rand_color())
+        rand_num_pool.append(rand_color_value())
 
+    #generate string to pass as CSS background-color property
     color1 = f"rgb({','.join(str(x) for x in rand_num_pool[:3])})"
     color2 = f"rgb({','.join(str(x) for x in rand_num_pool[-3:])})"
 
-    return render_template("index.html", x= rand_coord(), y= rand_coord(), color1= color1, color2= color2)
+    return render_template("index.html", x= rand_coord_value(), y= rand_coord_value(), color1= color1, color2= color2)
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
