6b166f7600726399494e7904b29cbabac52ffb64
--global <--global>
2021-10-22 09:16:51 -0500

    correct python styling

--- app.py
+++ app.py
@@ -13,23 +13,23 @@ def errorhandler(err):
 @app.route('/<int:x>/<int:y>')
 @app.route('/<int:x>/<int:y>/<color1>')
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
-def gen_checkerboard(x= 8, y= 8, color1= "red", color2= "black"):
+def get_checkerboard(x= 8, y= 8, color1= "red", color2= "black"):
     return render_template("index.html", x= x, y= y, color1= color1, color2= color2)
 
 @app.route('/r')
-def random_board():
+def get_random_checkerboard():
 
     #random coordinate for x,y
-    get_coord = lambda : round(random.random()*(20-1) + 1)
+    def _get_coord(): return round(random.random()*(20-1) + 1)
 
-    #random rgb sub value (i.e. rgb(r,r,r))
-    get_rgb = lambda : round(random.random()*255 - 0)
+    #random sub-value(x) for rgb color (i.e. rgb(x,x,x))
+    def _get_rgb(): return round(random.random()*255 - 0)
 
     #generate string to pass as CSS background-color property
-    color1 = f"rgb({get_rgb()}, {get_rgb()}, {get_rgb()})"
-    color2 = f"rgb({get_rgb()}, {get_rgb()}, {get_rgb()})"
+    color1 = f"rgb({_get_rgb()}, {_get_rgb()}, {_get_rgb()})"
+    color2 = f"rgb({_get_rgb()}, {_get_rgb()}, {_get_rgb()})"
 
-    return render_template("index.html", x= get_coord(), y= get_coord(), color1= color1, color2= color2)
+    return render_template("index.html", x= _get_coord(), y= _get_coord(), color1= color1, color2= color2)
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
